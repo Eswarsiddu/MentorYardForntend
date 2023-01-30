52d1a603da82ae6b5c28be5d4b145a71050ff007
vishalkrsoni <vishal.sony1@gmail.com>
2023-01-30 15:26:15 +0530

    nav logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,15 @@
 import "./NavBar.css";
+import logoIcon from '../../assets/logoDesigns/svg/logo-no-background.svg'
+
+
 export function NavBar() {
-  return <nav>NavBar</nav>;
+  return <nav id="nav-container" className="nav__container">
+
+    <img src={logoIcon} alt="Logo Image upload failed" id="nav-icon" className="nav__icon"/>
+    <div className="nav__options">
+      <div className="nav__option">Home</div>
+      <div className="nav__option">About Us</div>
+      <div className="nav__option">Contact Us</div>
+    </div>
+  </nav>;
 }
