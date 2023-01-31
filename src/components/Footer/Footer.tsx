13101f8e9ebdcb8bd1a012c25632c60ecc1e3996
vishalkrsoni <vishal.sony1@gmail.com>
2023-01-31 21:26:16 +0530

    basic footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,51 +1,56 @@
 import React from "react";
-import './Footer.css'
+import "./Footer.css";
 function Footer() {
   return (
     <div className="footer__container">
       <div className="footer__icons">
         icons here
-      <div className="footer__icons__options">
-      <a href="https://www.facebook.com/vishalkrsoni">
-        <i className="fa fa-facebook-square" />
-      </a>
-      <a href="https://myaccount.google.com/profile">
-        <i className="fa fa-google-plus-square" />
-      </a>
-      <a href="https://www.instagram.com/vishalkrsoni/">
-        <i className="fa fa-instagram" />
-      </a>
-      <a href="https://github.com/vishalkrsoni">
-        <i className="fa fa-github"></i>
-      </a>
-      <a href="https://www.linkedin.com/in/vishal-soni-a8ab14a9/">
-        <i className="fa fa-linkedin-square"></i>
-      </a>
-      </div>
-      
+        <div className="footer__icons__options">
+          <a href="https://www.facebook.com/vishalkrsoni">
+            <i className="fa fa-facebook-square" />
+          </a>
+          <a href="https://myaccount.google.com/profile">
+            <i className="fa fa-google-plus-square" />
+          </a>
+          <a href="https://www.instagram.com/vishalkrsoni/">
+            <i className="fa fa-instagram" />
+          </a>
+          <a href="https://github.com/vishalkrsoni">
+            <i className="fa fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/vishal-soni-a8ab14a9/">
+            <i className="fa fa-linkedin-square"></i>
+          </a>
+        </div>
       </div>
 
       <div className="footer__contact__info">
-      contact info
-      <div className="footer__contact__options">
-      <div className="footer__contact__number">+91-7838516299</div>
-      <div className="footer__contact__email">vishal.sony1@gmail.com</div>
-      <div className="footer__contact__address">Bangalore,India</div>
+        contact info :
+        <div className="footer__contact__options">
+          <div className="footer__contact number">
+            <i className="uil uil-phone-volume contact__icon"></i>
 
+            <span> +91-7838516299</span>
+          </div>
+          <div className="footer__contact email">
+            <i className="uil uil-envelope-edit contact__icon"></i>
+            <span>vishal.sony1@gmail.com</span>
+          </div>
+          <div className="footer__contact address">
+            <i className="uil uil-map-marker-alt contact__icon"></i>
+            <span>Bangalore,India</span>
+          </div>
+        </div>
       </div>
 
-      </div>
-     
       <div className="footer__navigate">
         Navigate to :
         <div className="footer__navigate__options">
-        <div className="footer__contactUs">contact Us</div>
-        <div className="footer__aboutUs">about us</div>
-        <div className="footer__testimonial">testimonial</div>
-        <div className="footer__montors">Our Mentors</div>
-
+          <div className="footer__contactUs tab">contact Us</div>
+          <div className="footer__aboutUs tab">about us</div>
+          <div className="footer__testimonial tab">testimonial</div>
+          <div className="footer__montors tab">Our Mentors</div>
         </div>
-       
       </div>
     </div>
   );
