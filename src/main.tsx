14673fdfbaf15b34f975e5a9fad85ada649eaf2e
vishalkrsoni <vishal.sony1@gmail.com>
2023-02-02 15:11:01 +0530

    added logo and new header

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.css";
 import NavBar from "./components/NavBar/NavBar";
+import Header from "./components/Header/Header";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <NavBar />
+      <Header />
       <App />
     </BrowserRouter>
   </React.StrictMode>
