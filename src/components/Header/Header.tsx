f0b063c8556eb9f4e060ddb26a688dd113ef866d
vishalkrsoni <vishal.sony1@gmail.com>
2023-02-07 20:33:57 +0530

    temp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import './Header.css'
 import React from 'react'
+import './Header.css'
 // import companyLogo from '../../assets/Images/logoDesigns/pngLogo.png'
 import companyLogo from '../../assets/Images/logoDesigns/finalLogo.svg'
 
@@ -9,7 +9,7 @@ function Header() {
     <div className='header__container'>
       <div className="header">
         <a href="#default" className="header__logo">
-          <img src={companyLogo} alt="logo" width={400} height={200}  className='company__logo'/>
+          <img src={companyLogo} alt="logo" width={400} height={200} className='company__logo' />
         </a>
         <div className="header-right">
           <a className="header__tab home__tab active" href="#home" >Home</a>
@@ -18,7 +18,6 @@ function Header() {
           <a className="header__tab contact__tab " href="#about">Contact</a>
           <a className="header__tab login__tab " href="#about">Login</a>
           <a className="header__tab signup__tab " href="#about">Sign Up</a>
-
         </div>
       </div></div>
   )
