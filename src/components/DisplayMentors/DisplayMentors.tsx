0a4aa3535a9581bf90c7519192a8ce7ed3cabb64
vishalkrsoni <vishal.sony1@gmail.com>
2023-02-10 10:14:29 +0530

    added mentor display flipping card

--- src/components/DisplayMentors/DisplayMentors.tsx
+++ src/components/DisplayMentors/DisplayMentors.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './DisplayMentors.css'
 import { FlippingCard } from '../FlippingCard/FlippingCard'
+// import SlidingDisplayCards from '../SlidingDisplayCard/SlidingDisplayCard'
 
 
 function DisplayMentors() {
   return (
     <div className='display__mentors__container'>
+      {/* <SlidingDisplayCards /> */}
       <FlippingCard />
       <FlippingCard />
       <FlippingCard />
       <FlippingCard />
       <FlippingCard />
+
 
       </div>
   )
--- src/components/SlidingDisplayCard/SlidingDisplayCard.tsx
+++ src/components/SlidingDisplayCard/SlidingDisplayCard.tsx
@@ -1,39 +1,43 @@
-import React from "react";
+
+import React, { useState, useEffect } from 'react';
+import './SlidingDisplayCards.css';
 import frontImage from "../../../public/Mentors/AntMan.jpeg";
 
-interface SlidingDisplayCardProps {
-  delay: number;
+
+interface User {
+  id: number;
+  name: string;
+  avatar: string;
 }
 
-const SlidingDisplayCard: React.FC<SlidingDisplayCardProps> = ({ delay }) => {
-  return (
-    <div className="card" style={{ display: "flex", "--delay": delay }}>
-      <div className="content">
-        <div className="img">
-          <img src={frontImage} alt="" />
-        </div>
-        <div className="details">
-          <span className="name">Name</span>
-          <p>Position</p>
-        </div>
-      </div>
-      <a href="#">Follow</a>
-    </div>
-  );
-};
+interface Props {
+  users: User[];
+}
 
-const SlidingDisplayCards: React.FC = () => {
+const SlidingDisplayCards: React.FC<Props> = ({ users }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState<any>(0);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % users.length);
+    }, 3000);
+    setIntervalId(id);
+    return () => clearInterval(intervalId);
+  }, [currentIndex, users.length, intervalId]);
+
   return (
-    <div className="slider__container">
-      <div className="wrapper">
-        <div className="outer">
-          <SlidingDisplayCard delay={-1} />
-          <SlidingDisplayCard delay={0} />
-          <SlidingDisplayCard delay={1} />
-          <SlidingDisplayCard delay={2} />
-          <SlidingDisplayCard delay={2} />
-        </div>
-      </div>
+    <div className="sliding-display-cards">
+      {users.map((user, index) => {
+        const isActive = index === currentIndex;
+        const className = isActive ? 'sliding-card active' : 'sliding-card';
+        return (
+          <div key={user.id} className={className}>
+            <img src={user.avatar} alt={user.name} />
+            <p>{user.name}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
